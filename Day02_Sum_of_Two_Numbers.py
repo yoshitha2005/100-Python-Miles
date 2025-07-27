90a5b5a0573ca13639d18f7f57f2c314afe48eb2
Kothapalli Yoshitha Devi <143061124+yoshitha2005@users.noreply.github.com>
2025-07-27 20:54:58 +0530

    Update Day02_Sum_of_Two_Numbers.py

--- Day02_Sum_of_Two_Numbers.py
+++ Day02_Sum_of_Two_Numbers.py
@@ -1,3 +1,10 @@
 a=int(input("Enter a number : "))
 b=int(input("Enter a number : "))
 sum=print(a+b)
+//Using functions
+def add(a,b):
+  return a+b
+n1=int(input("enter 1st num : "))
+n2=int(input("enter 2nd num : "))
+result=add(n1,n2)
+print(f"The sum of 2 numbers {n1} and {n2} is {result}")
