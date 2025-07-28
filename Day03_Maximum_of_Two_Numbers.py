8f72ad7f3a13b3b4fc9c8c2d81280e3806013b8f
Kothapalli Yoshitha Devi <143061124+yoshitha2005@users.noreply.github.com>
2025-07-28 16:26:44 +0530

    Add files via upload

---
+++ Day03_Maximum_of_Two_Numbers.py
@@ -0,0 +1,8 @@
+def max(a,b):
+    if(a>b):
+        print("A is greater")
+    else:
+        print("B is greater")
+num1=int(input("ënter a num : "))
+num2=int(input("ënter a num : "))
+result=max(num1,num2)
