03ef5d64e1f999ad95ac1a6e555e030983db2db8
Kothapalli Yoshitha Devi <143061124+yoshitha2005@users.noreply.github.com>
2025-07-28 21:09:44 +0530

    Add files via upload

---
+++ Day04_Check_Even_or_Odd.py
@@ -0,0 +1,7 @@
+def evenorodd(num):
+    if(num%2==0):
+        print(f"The given number {num} is an even number")
+    else:
+        print(f"The given number {num} is an odd number")
+a=int(input("Enter a number : "))
+result=evenorodd(a)
