74e90445f245056bac5ed5151350032f2c3f45c4
Kothapalli Yoshitha Devi <143061124+yoshitha2005@users.noreply.github.com>
2025-08-06 10:37:28 +0530

    Update Day17_Celsius_to_Fahrenheit.py

---
+++ Day17_Celsius_to_Fahrenheit.py
@@ -0,0 +1,5 @@
+def Celsius_to_Fahrenheit():
+    celsius=float(input("Enter a temperature:"))
+    Fahrenheit=(celsius*9/5)+32
+    print(f"Celsius {celsius}°C equal to Fahrenheit is:{Fahrenheit}°F")
+Celsius_to_Fahrenheit()
